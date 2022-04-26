20f857f0af4a599c11d0edeee53b89ac56aae114
Aidan <103147872+abarciab@users.noreply.github.com>
2022-04-26 09:14:53 -0700

    improved duck flip

--- src/prefabs/Duck.js
+++ src/prefabs/Duck.js
@@ -12,8 +12,8 @@ class Duck extends Phaser.GameObjects.Sprite {
         this.quack.setVolume(0.7);
         this.quack.play();
         this.can_collide = true;
-        this.turnCounter = 120;
-        this.maxCountdown = 120;
+        this.turnCounter = 200;
+        this.maxCountdown = 200;
     }
     flyAway(player) {
         this.can_collide = false;
@@ -25,9 +25,18 @@ class Duck extends Phaser.GameObjects.Sprite {
     }
     update() {
         this.turnCounter -= 1;
-        if (this.turnCounter <= 0){
+        if (this.turnCounter <= 0 && this.x <= game.config.width/2 && this.flipping != false){
             this.moveSpeed *= -1;
-            this.turnCounter = this.maxCountdown * 1.5;
+            this.turnCounter = this.maxCountdown;
+            if (this.moveSpeed < 0){
+                this.flipX = true;   
+            }
+            this.maxCountdown *= 0.9;
+            if (this.maxCountdown <= 30){
+                this.flipping = false;
+                this.moveSpeed = Math.abs(this.moveSpeed);
+                this.flipX = false;
+            }
         }
         if (this.can_collide)
             this.x -= this.moveSpeed;
